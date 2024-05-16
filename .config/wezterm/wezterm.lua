727b379fc8f99e08bb459fc1f7b3fa11522bb2ea
Robert Cottrell <rgcottrell@gmail.com>
2024-05-16 12:37:10 +0100

    Update some configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,12 +9,12 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Macchiato"
 
 config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 20
+config.font_size = 19
 
 config.enable_tab_bar = false
 
 config.window_decorations = "TITLE | RESIZE"
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.macos_window_background_blur = 8
 
 -- and finally, return the configuration to wezterm
--- .zshrc
+++ .zshrc
@@ -1,6 +1,9 @@
 export EDITOR="nvim"
 
 alias ls="lsd"
+alias la="ls -a"
+alias ll="ls -l"
+alias lla="ls -la"
 
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
